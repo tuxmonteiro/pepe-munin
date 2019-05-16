c49083039f3891a760c8ad11b2f29167015fc987
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-16 18:54:57 -0300

    Using MockServer and improve tests

--- src/test/java/com/globo/pepe/munin/service/MuninServiceTest.java
+++ src/test/java/com/globo/pepe/munin/service/MuninServiceTest.java
@@ -1,53 +1,132 @@
 package com.globo.pepe.munin.service;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.globo.pepe.common.services.JsonLoggerService;
 import com.globo.pepe.munin.repository.SofiaRepository;
-import com.globo.pepe.munin.util.KeystoneMock;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.openstack4j.api.OSClient.OSClientV3;
+import org.mockserver.integration.ClientAndServer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest({MuninService.class, PepeApiService.class, KeystoneService.class, JsonLoggerService.class, ObjectMapper.class})
 public class MuninServiceTest {
 
     @Autowired
     private MuninService muninService;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private SofiaRepository sofiaRepository;
 
     @MockBean
-    private KeystoneService keystoneService;
+    private PepeApiService pepeApiService;
+
+    private JsonNode metricValueJson = null;
+
+    public static ClientAndServer keystoneServerMock;
+
+    @BeforeClass
+    public static void setupClass() throws IOException {
+        keystoneServerMock = ClientAndServer.startClientAndServer(5000);
+
+        InputStream resourceAuthOk = MuninServiceTest.class.getResourceAsStream("/keystone-auth.json");
+        String bodyAuthOk = IOUtils.toString(resourceAuthOk, Charset.defaultCharset());
+        keystoneServerMock.when(request().withMethod("POST").withPath("/v3/auth/tokens").withBody(requestBodyWithUserPassword("user", "password")))
+            .respond(response().withBody(bodyAuthOk).withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(201));
+
+        InputStream resourceAuthFail = MuninServiceTest.class.getResourceAsStream("/keystone-auth-fail.json");
+        String bodyAuthFail = IOUtils.toString(resourceAuthFail, Charset.defaultCharset());
+        keystoneServerMock.when(request().withMethod("POST").withPath("/v3/auth/tokens").withBody(requestBodyWithUserPassword("user-wrong", "password-wrong")))
+            .respond(response().withBody(bodyAuthFail).withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(401));
+
+        String bodyKeystoneError = "";
+        keystoneServerMock.when(request().withMethod("POST").withPath("/v3/auth/tokens").withBody(requestBodyWithUserPassword("force-error", "")))
+            .respond(response().withBody(bodyKeystoneError).withHeader("Content-Type", APPLICATION_JSON_VALUE).withStatusCode(201));
+
+    }
+
+    private static String requestBodyWithToken(String token) {
+        return "{\n"
+            + "  \"auth\" : {\n"
+            + "    \"identity\" : {\n"
+            + "      \"token\" : {\n"
+            + "        \"id\" : \"" + token + "\"\n"
+            + "      },\n"
+            + "      \"methods\" : [ \"token\" ]\n"
+            + "    },\n"
+            + "    \"scope\" : {\n"
+            + "      \"project\" : {\n"
+            + "        \"name\" : \"admin\",\n"
+            + "        \"domain\" : {\n"
+            + "          \"name\" : \"default\"\n"
+            + "        }\n"
+            + "      }\n"
+            + "    }\n"
+            + "  }\n"
+            + "}";
+    }
+
+    private static String requestBodyWithUserPassword(String user, String password) {
+        return "{\n"
+            + "  \"auth\" : {\n"
+            + "    \"identity\" : {\n"
+            + "      \"password\" : {\n"
+            + "        \"user\" : {\n"
+            + "          \"name\" : \"" + user + "\",\n"
+            + "          \"domain\" : {\n"
+            + "            \"id\" : \"default\"\n"
+            + "          },\n"
+            + "          \"password\" : \"" + password + "\"\n"
+            + "        }\n"
+            + "      },\n"
+            + "      \"methods\" : [ \"password\" ]\n"
+            + "    }\n"
+            + "  }\n"
+            + "}";
+    }
 
     @Before
-    public void initMocks() throws Exception {
+    public void setup() {
         List<Map<String, Object>> metrics = new ArrayList<>();
-        Map<String,Object> metricValue = new LinkedHashMap<>();
+        Map<String, Object> metricValue = new LinkedHashMap<>();
         metricValue.put("cpu","00");
         metricValue.put("ram","00");
         metricValue.put("so","linux");
         metrics.add(metricValue);
+        metricValueJson = mapper.valueToTree(metricValue);
 
-        Mockito.when(this.sofiaRepository.findByMetrics(Mockito.anyString())).thenReturn(metrics);
-        OSClientV3 osClientV3 = KeystoneMock.getOSClientV3();
-        Mockito.when(keystoneService.authenticate()).thenReturn(osClientV3);
-        MockitoAnnotations.initMocks(this);
+        when(sofiaRepository.findByMetrics(anyString())).thenReturn(metrics);
     }
 
     @Test
-    public void sendMetrics() throws Exception {
+    public void sendWithUserAndPasswordOkTest() {
         muninService.send();
+        verify(pepeApiService, Mockito.atLeastOnce()).sendMetrics(metricValueJson, "admin", null);
     }
 
 }
