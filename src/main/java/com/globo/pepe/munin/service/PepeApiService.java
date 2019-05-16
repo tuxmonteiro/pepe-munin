3262c6ac4b4735be3194872ca6ec2a6307c4d0d3
felipe.oliveira <felipe.oliveira@corp.globo.com>
2019-05-16 10:01:11 -0300

    Remove ignore ssl

--- src/main/java/com/globo/pepe/munin/service/PepeApiService.java
+++ src/main/java/com/globo/pepe/munin/service/PepeApiService.java
@@ -78,33 +78,8 @@ public class PepeApiService {
 
     @Bean
     public RestTemplate getRestTemplate() {
-
-
-
-        try {
-            SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy).build();
-            SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
-            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-            requestFactory.setHttpClient(httpClient);
-            RestTemplate restTemplate = new RestTemplate(requestFactory);
-
-            return restTemplate;
-        }catch (Exception e){
             RestTemplate restTemplate = new RestTemplate();
-
-            List<HttpMessageConverter<?>> list = new ArrayList<>();
-            list.add(new MappingJackson2HttpMessageConverter());
-            restTemplate.setMessageConverters(list);
             return restTemplate;
-        }
     }
 
-    TrustStrategy acceptingTrustStrategy = new TrustStrategy() {
-        @Override
-        public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-            return true;
-        }
-    };
 }
