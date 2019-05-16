1a6cd382eadf99cc6141b5a6f38bc12674b8ad59
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-16 15:23:11 -0300

    Refactoring: remove MuninRunner

--- src/main/java/com/globo/pepe/munin/Application.java
+++ src/main/java/com/globo/pepe/munin/Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Import({JsonLoggerService.class})
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/globo/pepe/munin/service/MuninService.java
+++ src/main/java/com/globo/pepe/munin/service/MuninService.java
@@ -2,11 +2,13 @@ package com.globo.pepe.munin.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.globo.pepe.common.services.JsonLoggerService;
 import com.globo.pepe.munin.repository.SofiaRepository;
 import java.util.List;
 import java.util.Map;
 import org.openstack4j.api.OSClient.OSClientV3;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,29 +21,37 @@ public class MuninService {
     private final PepeApiService pepeApiService;
     private final KeystoneService keystoneService;
     private final ObjectMapper mapper;
+    private final JsonLoggerService jsonLoggerService;
 
     public MuninService(SofiaRepository sofiaRepository,
-            PepeApiService pepeApiService,
-            KeystoneService keystoneService,
-            ObjectMapper mapper) {
+        PepeApiService pepeApiService,
+        KeystoneService keystoneService,
+        ObjectMapper mapper,
+        JsonLoggerService jsonLoggerService) {
+
         this.sofiaRepository = sofiaRepository;
         this.pepeApiService = pepeApiService;
         this.keystoneService = keystoneService;
         this.mapper = mapper;
+        this.jsonLoggerService = jsonLoggerService;
     }
 
-    public void send() throws Exception {
-        OSClientV3 os = keystoneService.authenticate();
-        String project = os.getToken().getProject().getName();
-        String tokenId = os.getToken().getId();
+    @Scheduled(fixedDelayString = "${pepe.munin.fixedDelay}")
+    public void send() {
+        try {
+            OSClientV3 os = keystoneService.authenticate();
+            String project = os.getToken().getProject().getName();
+            String tokenId = os.getToken().getId();
 
-        final List<Map<String, Object>> metrics = sofiaRepository.findByMetrics(queryWorker);
+            final List<Map<String, Object>> metrics = sofiaRepository.findByMetrics(queryWorker);
 
-        for(Map<String, Object> metric : metrics) {
-            JsonNode jsonNode = mapper.valueToTree(metric);
-            pepeApiService.sendMetrics(jsonNode, project, tokenId);
+            for (Map<String, Object> metric : metrics) {
+                JsonNode jsonNode = mapper.valueToTree(metric);
+                pepeApiService.sendMetrics(jsonNode, project, tokenId);
+            }
+        } catch (Exception e){
+            jsonLoggerService.newLogger(getClass()).message(e.getMessage()).sendError();
         }
-
     }
 
 }
