f45b1d41555395ee86f6a4544cee65983161eb87
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2019-05-19 18:06:04 -0300

    Keystone Client: Add Expires controller and fix tests

--- src/main/java/com/globo/pepe/munin/service/KeystoneService.java
+++ src/main/java/com/globo/pepe/munin/service/KeystoneService.java
@@ -7,6 +7,10 @@ import org.openstack4j.openstack.OSFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.AbstractMap;
+import java.util.Date;
+
 @Service
 public class KeystoneService {
 
@@ -25,16 +29,23 @@ public class KeystoneService {
     @Value("${pepe.keystone.project}")
     private String project;
 
-    public OSClientV3 authenticate() throws Exception {
-        OSClientV3 client = OSFactory.builderV3()
-                .endpoint(keystoneEndPoint)
-                .credentials(user, password, null)
-                .scopeToProject(Identifier.byName(project), Identifier.byId(domain))
-                .authenticate();
-        if (client == null) {
-            throw new AuthenticationException("client is null", 401);
+    private AbstractMap.Entry<OSClientV3, Date> osClientWithExpires = new AbstractMap.SimpleEntry<>(null, Date.from(Instant.now()));
+
+    public synchronized OSClientV3 authenticate() throws Exception {
+        if (osClientWithExpires.getKey() == null || osClientWithExpires.getValue().before(Date.from(Instant.now()))) {
+            final OSClientV3 osClient = OSFactory.builderV3()
+                    .endpoint(keystoneEndPoint)
+                    .credentials(user, password, Identifier.byId(domain))
+                    .scopeToProject(Identifier.byName(project), Identifier.byId(domain))
+                    .authenticate();
+            if (osClient == null || osClient.getToken() == null) {
+                throw new AuthenticationException("client is null", 401);
+            } else {
+                final Date expires = osClient.getToken().getExpires();
+                osClientWithExpires = new AbstractMap.SimpleEntry<>(osClient, expires);
+            }
         }
-        return client;
+        return osClientWithExpires.getKey();
     }
 
 }
